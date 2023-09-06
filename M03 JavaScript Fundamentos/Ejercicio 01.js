d725b969f10317e1fcc1fca98d9f1d4b888189cf
Nazareno Lozano <lozanonazareno@gmail.com>
2023-09-05 21:50:34 -0300

    ejercicios 1 y 2

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -4,23 +4,23 @@
 // En los siguientes ejercicios deberás reemplazar el valor null
 // por el correspondiente.
 
-// Crea una variable de tipo string.
-const nuevoString = null;
+// 1 Crea una variable de tipo string.
+const nuevoString = "Naza";
 
-// Crea una variable de tipo number.
-const nuevoNumero = null;
+// 2 Crea una variable de tipo number.
+const nuevoNumero = 45;
 
-// Crea una variable de tipo boolean.
-const nuevoBoolean = null;
+// 3 Crea una variable de tipo boolean.
+const nuevoBoolean = true;
 
-// Resuelve el siguiente problema matemático.
-const nuevaResta = 10 - null === 3;
+// 4 Resuelve el siguiente problema matemático.
+const nuevaResta = 10 - 7 === 3;
 
-// Resuelve el siguiente problema matemático.
-const nuevaMultiplicacion = 10 * null === 40;
+// 5 Resuelve el siguiente problema matemático.
+const nuevaMultiplicacion = 10 * 4 === 40;
 
-// Resuelve el siguiente problema matemático.
-const nuevoModulo = 21 % 5 === null;
+// 6 Resuelve el siguiente problema matemático.
+const nuevoModulo = 21 % 5 === 1;
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -3,34 +3,40 @@
 
 function devolverString(string) {
    // Debe retornar un string.
-   // Tu código:
+   // 111 Tu código:
+   return string
 }
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
-   // Tu código:
+   // 222 Tu código:
+   return x+y
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
-   // Tu código:
+   // 333 Tu código:
+   return x-y
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
-   // Tu código:
+   // 444 Tu código:
+   return x/y
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
-   // Tu código:
+   // 555 Tu código:
+   return x*y
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
-   // Tu código:
+   // 666 Tu código:
+   return x%y
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
